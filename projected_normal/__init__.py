e0bb5c36f08d2ec4b09aebe419305b831d98a112
dherresp-adm <dherrera1911@gmail.com>
2025-02-24 15:07:05 -0500

    New init file

--- projected_normal/__init__.py
+++ projected_normal/__init__.py
@@ -1,7 +1,6 @@
 # __init__.py
-from .auxiliary import *
-from .qf_func import *
-from .prnorm_func import *
-from .prnorm_class import *
-from .parametrizations import *
-
+from . import quadratic_forms as quadratic_forms
+from . import distribution as distribution
+from . import param_sampling as param_sampling
+from . import matrix_checks as matrix_checks
+from . import models as models
