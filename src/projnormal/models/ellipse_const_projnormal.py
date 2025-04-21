443a82340d63856f48471790cf39c7df5c6941ab
dherresp-adm <dherrera1911@gmail.com>
2025-04-21 18:35:31 -0400

    Add fixed B const class

--- src/projnormal/models/ellipse_const_projnormal.py
+++ src/projnormal/models/ellipse_const_projnormal.py
@@ -6,7 +6,7 @@ import torch.nn.utils.parametrize as parametrize
 import projnormal.distribution as prnorm
 
 from ._constraints import Positive
-from .ellipse_projnormal import ProjNormalEllipse, ProjNormalEllipseIso
+from .ellipse_projnormal import ProjNormalEllipse, ProjNormalEllipseIso, ProjNormalEllipseFixed
 
 
 __all__ = [
@@ -265,6 +265,136 @@ class ProjNormalEllipseConstIso(ProjNormalEllipseIso):
           B_sqrt_coefs=B_sqrt_coefs,
           B_sqrt_vecs=B_sqrt_vecs,
           B_sqrt_diag=B_sqrt_diag,
+        )
+
+        # Parse const
+        if const is None:
+            const = torch.tensor(1.0)
+        elif not torch.is_tensor(const) or const.dim() != 0 or const <= 0:
+            if const.dim() == 1 and const.numel() == 1:
+                const = const.squeeze()
+            else:
+                raise ValueError("const must be a positive scalar tensor.")
+
+        self.const = nn.Parameter(const.clone())
+        parametrize.register_parametrization(self, "const", Positive())
+
+
+    def log_pdf(self, y):
+        """
+        Compute the log pdf at points y under the projected normal distribution.
+
+        Parameters
+        ----------------
+          y : torch.Tensor, shape (n_points, n_dim)
+              Points to evaluate the log pdf.
+
+        Returns
+        ----------------
+          torch.Tensor, shape (n_points)
+              Log PDF of the point. (n_points)
+        """
+        # Extract B matrices needed
+        B = self.ellipse.get_B()
+        B_sqrt = self.ellipse.get_B_sqrt()
+        B_sqrt_ldet = self.ellipse.get_B_sqrt_inv()
+
+        lpdf = prnorm.ellipse_const.pdf.log_pdf(
+            mean_x=self.mean_x,
+            covariance_x=self.covariance_x,
+            y=y,
+            const=self.const,
+            B=B,
+            B_sqrt=B_sqrt,
+            B_sqrt_ldet=B_sqrt_ldet
+        )
+        return lpdf
+
+    def __dir__(self):
+        return super().__dir__() + ["const"]
+
+
+class ProjNormalEllipseConstFixed(ProjNormalEllipseFixed):
+    """
+    This class implements the general projected normal distribution but with
+    projection to the interior of an ellipse.
+    The variable Y following the distribution
+    is defined as Y = X / sqrt(X'BX + const), where X~N(mean_x, covariance_x),
+    B is a symmetric positive definite matrix, and const is a positive scalar.
+    The class can be used to fit distribution parameters to data.
+
+    Attributes
+    -----------
+      mean_x : torch.Tensor, shape (n_dim)
+          Mean of X. It is constrained to the unit sphere.
+
+      sigma2 : torch.Tensor, shape (n_dim, n_dim), optional
+          Initial covariance. Default is the identity.
+
+      const : torch.Tensor, shape (1)
+          The const denominator constant. It is constrained to be positive.
+
+      B : torch.Tensor, shape (n_dim, n_dim)
+          The ellipse matrix. It is constrained to be symmetric positive definite.
+
+    Methods
+    ----------
+      moments():
+          Compute the moments using a Taylor approximation.
+
+      log_pdf() :
+          Compute the value of the log pdf at given points.
+
+      pdf() :
+          Compute the value of the pdf at given points.
+
+      moments_empirical() :
+          Compute the moments by sampling from the distribution
+
+      sample() :
+          Sample points from the distribution.
+
+      moment_match() :
+          Fit the distribution parameters to the observed moments.
+
+      max_likelihood() :
+          Fit the distribution parameters to the observed data
+          using maximum likelihood.
+    """
+
+    def __init__(
+        self,
+        n_dim=None,
+        mean_x=None,
+        covariance_x=None,
+        B=None,
+        const=None,
+    ):
+        """Initialize an instance of the ProjNormal class.
+
+        Parameters
+        ------------
+          n_dim : int, optional
+              Dimension of the underlying Gaussian distribution. If mean
+              and covariance are provided, this is not required.
+
+          mean_x : torch.Tensor, shape (n_dim), optional
+              Mean of X. It is converted to unit norm. Default is random.
+
+          covariance_x : torch.Tensor, shape (n_dim, n_dim), optional
+              Initial covariance. Default is the identity.
+
+          const : torch.Tensor, shape (1), optional
+              The const denominator constant. Default is 1.
+
+          B : torch.Tensor, shape (n_dim, n_dim), optional
+              The ellipse matrix. Default is the identity.
+        """
+        super().__init__(
+          n_dim=n_dim,
+          mean_x=mean_x,
+          covariance_x=covariance_x,
+          B=B,
         )
 
         # Parse const
