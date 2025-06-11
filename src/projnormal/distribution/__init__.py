83a40da40393d057c5057dfe807c93c54cdac0f6
dherresp-adm <dherrera1911@gmail.com>
2025-06-11 17:42:55 -0400

    Change name of projected_normal submodule

--- src/projnormal/distribution/__init__.py
+++ src/projnormal/distribution/__init__.py
@@ -1,5 +1,5 @@
 from . import const as const
 from . import ellipse as ellipse
 from . import ellipse_const as ellipse_const
-from . import general as general
 from . import isotropic as isotropic
+from . import projected_normal as projected_normal
