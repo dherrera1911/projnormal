481aa473dac9700b6e8ee8ab34c129ae32adf884
dherresp-adm <dherrera1911@gmail.com>
2025-06-16 15:51:20 -0400

    Make constraints module visible

--- src/projnormal/models/__init__.py
+++ src/projnormal/models/__init__.py
@@ -3,12 +3,14 @@ from .projected_normal import ProjNormal as ProjNormal
 from .const import ProjNormalConst as ProjNormalConst
 from .ellipse import ProjNormalEllipse as ProjNormalEllipse
 from .ellipse_const import ProjNormalEllipseConst as ProjNormalEllipseConst
+from . import constraints as constraints
 
 __all__ = [
   "ProjNormal",
   "ProjNormalConst",
   "ProjNormalEllipse",
   "ProjNormalEllipseConst",
+  "constraints",
 ]
 
 def __dir__():
